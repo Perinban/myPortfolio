a3796c371ef907777c8aa4a3b5a848464feafed7
Perinban <Perinban@users.noreply.github.com>
2026-08-23 11:14:22 +0000

    deploy: 2aa77eacc4542db3d5d1b3f5fd0cb611dbe756b9

--- assets/extends-DMIsdI2l.js
+++ assets/extends-DMIsdI2l.js
@@ -1,4 +1,4 @@
-import{r as Qe,a as wx,g as Ex,j as yi}from"./index-2HsWKy6Y.js";/**
+import{r as Qe,a as wx,g as Ex,j as yi}from"./index-DkR23jMu.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
